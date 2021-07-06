9bfdadfea831ca3480081a1d0fffc9dda401121b
serapbaysal <serap.baysal@hotmail.com>
2021-07-06 14:11:45 +0300

    started project

---
+++ HotelFinder/Hotel/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hotel
+{
+    public class Hotel
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+    }
+}
