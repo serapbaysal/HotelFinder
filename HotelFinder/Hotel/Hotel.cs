393b4301e2b3e5681c5e5a46b092771ea6bc8ac6
serapbaysal <serap.baysal@hotmail.com>
2021-07-06 15:40:28 +0300

    updated horel

--- HotelFinder/Hotel/Hotel.cs
+++ HotelFinder/Hotel/Hotel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Hotel
 {
     public class Hotel
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id
         {
             get;
